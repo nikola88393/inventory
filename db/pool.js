0400efe663d13e54a96f4340a5d00cdc75a1564d
nikola <nikola88393@gmail.com>
2025-02-10 23:57:49 +0200

    add env variables support

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: "localhost",
-  user: "nikola",
-  database: "inventory",
-  password: "Nikimiho2",
+  host: process.env.DATABASE_HOST || "localhost",
+  user: process.env.DATABASE_USER || "nikola",
+  database: process.env.DATABASE_NAME || "inventory",
+  password: process.env.DATABASE_PASSWORD || "asdasdasd",
   port: 5432,
 });
